050eb6506442eee967dc8c7762935dba5374a60d
kiErnguyen <k@arbelo.io>
2023-04-19 21:23:46 -0700

    Interactive website quiz 95% complete!

--- Interactive_Website/js/game.js
+++ Interactive_Website/js/game.js
@@ -12,37 +12,100 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: "What is 2+2?",
-        choice1: "2",
-        choice2: "4",
-        choice3: "22",
-        answer: 2,
+        question: "What is your BMI?",
+        choice1: "0-15",
+        choice2: "16-30",
+        choice3: "30+",
+        answer: 3,
     },
     {
-        question: 'What is 2+2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '22',
-        answer: 2,
+        question: "Do you smoke?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
     },
     {
-        question: 'What is 2+2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '22',
-        answer: 2,
+        question: "Do you drink alcohol?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
     },
     {
-        question: 'What is 2+2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '22',
-        answer: 2,
+        question: "Have you had a stroke in the past?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To say",
+        answer: 1,
+    },
+    {
+        question: "Have you been having difficulty walking lately?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
+    },
+    {
+        question: "What is your sex?",
+        choice1: "Male",
+        choice2: "Female",
+        choice3: "Prefer Not To Say",
+        answer: 1,
+    },
+    {
+        question: "In which age range do you fall under?",
+        choice1: "18-30",
+        choice2: "31-60",
+        choice3: "60+",
+        answer: 3,
+    },
+    {
+        question: "Are you diabetic?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
+    },
+    {
+        question: "How would you describe your general health?",
+        choice1: "Outstanding",
+        choice2: "Good/ Very Good",
+        choice3: "Poor/ Fair",
+        answer: 3,
+    },
+    {
+        question: "How many hours of sleep do you get on average?",
+        choice1: "0-4",
+        choice2: "4-8",
+        choice3: "8+",
+        answer: 1,
+    },
+    {
+        question: "Do you have asthma?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
+    },
+    {
+        question: "Do you have kidney disease?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
+    },
+    {
+        question: "Do you have or have you ever had skin cancer?",
+        choice1: "Yes",
+        choice2: "No",
+        choice3: "Prefer Not To Say",
+        answer: 1,
     }
 ]
 
-const SCORE_POINTS = 50
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 25
+const MAX_QUESTIONS = 13
 
 startGame = () => {
     questionCounter = 0
